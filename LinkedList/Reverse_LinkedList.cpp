ef023fd625cf3d38dc5f9bba29554a6c0bd28966
rak626 <rakeshacot@gmail.com>
2023-06-25 15:12:22 +0530

    modifying reverse linked list using recursion

--- LinkedList/Reverse_LinkedList.cpp
+++ LinkedList/Reverse_LinkedList.cpp
@@ -2,13 +2,27 @@
 // Reverse Linked List
 ============================================*/
 
-LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) {
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head)
+{
   LinkedListNode<int> *prev = nullptr, *cur = head, *next = nullptr;
-  while (cur) {
+  while (cur)
+  {
     next = cur->next;
     cur->next = prev;
     prev = cur;
     cur = next;
   }
   return prev;
+}
+
+// using recursion
+LinkNode *reverse(ListNode *head)
+{
+  if (!head and !head->next)
+    return head;
+
+  ListNode *newHead = reverse(head->next);
+  head->next->next = head;
+  head->next = nullptr;
+  return newHead;
 }
